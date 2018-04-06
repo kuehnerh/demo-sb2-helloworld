01fedc5fa528f4fab2671b734c847cb822da8471
hkue0610 <heiko.kuehner@kaufland.com>
2018-04-06 10:06:17 +0200

    Initial Commit

---
+++ src/main/java/com/kaufland/demosb2helloworld/DemoSb2HelloworldApplication.java
@@ -0,0 +1,12 @@
+package com.kaufland.demosb2helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSb2HelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSb2HelloworldApplication.class, args);
+    }
+}
